25a7ab13b45b7b4592677b2dd657199759a2d250
Owain Webster <owainpw@gmail.com>
2020-12-20 15:29:59 +0000

    added basic structure to differnt sections of site

---
+++ js/main.js
@@ -0,0 +1,14 @@
+// maps
+var mymap = L.map('mapid', {
+    scrollWheelZoom: false
+}).setView([52.44, -4], 7.4);
+var marker = L.marker([52.44, -3.6]).addTo(mymap);
+
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1Ijoib3dlYnN0ZXIiLCJhIjoiY2tpeDd2ZWprM3F4cDJ5cWpzZnl5bTFtdyJ9.TkVm9dL3HOFz1F9m7fnaCA' //api key
+}).addTo(mymap);

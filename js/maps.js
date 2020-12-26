1d2aaf61a53aa9e6782abf4a79b883fae016711d
Owain Webster <owainpw@gmail.com>
2020-12-26 14:00:24 +0000

    added maps pop up when markers are selected

--- js/maps.js
+++ js/maps.js
@@ -14,20 +14,20 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
 }).addTo(mymap);
 
 function castleMarker(){ //when user clicks on castle button, add these markers to map to show where the castles are
-    var marker = L.marker([51.7696, -4.4620]).addTo(mymap); //Laugharne Castle
-    var marker = L.marker([53.1391, -4.2769]).addTo(mymap); //Caernarfon Castle
-    var marker = L.marker([51.7702, -2.8499]).addTo(mymap); //Raglan Castle
-    var marker = L.marker([51.8766, -4.0181]).addTo(mymap); //Dinefwr Park and Castle
-    var marker = L.marker([51.5931, -2.7423]).addTo(mymap); //Caldicot Castle
-    var marker = L.marker([53.2801, -3.8256]).addTo(mymap); //Conwy Castle
-    var marker = L.marker([52.6499, -3.1613]).addTo(mymap); //Powis Castle and Garden
-    var marker = L.marker([52.9354, -3.0894]).addTo(mymap); //Chirk Castle
-    var marker = L.marker([52.0572, -4.6342]).addTo(mymap); //Cilgerran Castle
-    var marker = L.marker([51.5761, -3.2202]).addTo(mymap); //Caerphilly Castle
+    var marker = L.marker([51.7696, -4.4620]).addTo(mymap).bindPopup('<p>Laugharne Castle</p>'); //Laugharne Castle
+    var marker = L.marker([53.1391, -4.2769]).addTo(mymap).bindPopup('<p>Caernarfon Castle</p>'); //Caernarfon Castle
+    var marker = L.marker([51.7702, -2.8499]).addTo(mymap).bindPopup('<p>Raglan Castle</p>'); //Raglan Castle
+    var marker = L.marker([51.8766, -4.0181]).addTo(mymap).bindPopup('<p>Dinefwr Park and Castle</p>'); //Dinefwr Park and Castle
+    var marker = L.marker([51.5931, -2.7423]).addTo(mymap).bindPopup('<p>Caldicot Castle</p>'); //Caldicot Castle
+    var marker = L.marker([53.2801, -3.8256]).addTo(mymap).bindPopup('<p>Conwy Castle</p>'); //Conwy Castle
+    var marker = L.marker([52.6499, -3.1613]).addTo(mymap).bindPopup('<p>Powis Castle and Garden</p>'); //Powis Castle and Garden
+    var marker = L.marker([52.9354, -3.0894]).addTo(mymap).bindPopup('<p>Chirk Castle</p>'); //Chirk Castle
+    var marker = L.marker([52.0572, -4.6342]).addTo(mymap).bindPopup('<p>Cilgerran Castle</p>'); //Cilgerran Castle
+    var marker = L.marker([51.5761, -3.2202]).addTo(mymap).bindPopup('<p>Caerphilly Castle</p>'); //Caerphilly Castle
 }
 
 function mountainsMarker(){
-    var marker = L.marker([53.0932, -3.8017]).addTo(mymap); //Snowdonia
-    var marker = L.marker([52.4167, -3.5000]).addTo(mymap); //Cambrian Mountains
-    var marker = L.marker([51.9667, -3.1167]).addTo(mymap); //Black Mountain Range
+    var marker = L.marker([53.0932, -3.8017]).addTo(mymap).bindPopup('<p>Snowdonia</p>'); //Snowdonia
+    var marker = L.marker([52.4167, -3.5000]).addTo(mymap).bindPopup('<p>Cambrian Mountains</p>'); //Cambrian Mountains
+    var marker = L.marker([51.9667, -3.1167]).addTo(mymap).bindPopup('<p>Black Mountain Range</p>'); //Black Mountain Range
 }

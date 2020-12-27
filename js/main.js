e6dd44ef5bd4eda4504583286ee420aebf5a44c9
Owain Webster <owainpw@gmail.com>
2020-12-27 15:23:56 +0000

    added basic smooth scrolling Jquery from stackoverflow for safari smooth scrolling

--- js/main.js
+++ js/main.js
@@ -1,3 +1,77 @@
+//smoothscrolling on safari
+(function($) {
+    $.fn.SmoothAnchors = function() {
+
+        function scrollBodyTo(destination, hash) {
+
+            // Change the hash first, then do the scrolling. This retains the standard functionality of the back/forward buttons.
+            var scrollmem = $(document).scrollTop();
+            window.location.hash = hash;
+            $(document).scrollTop(scrollmem);
+            $("html,body").animate({
+                scrollTop: destination
+            }, 200);
+
+        }
+
+        if (typeof $().on == "function") {
+            $(document).on('click', 'a[href^="#"]', function() {
+
+                var href = $(this).attr("href");
+
+                if ($(href).length == 0) {
+
+                    var nameSelector = "[name=" + href.replace("#", "") + "]";
+
+                    if (href == "#") {
+                        scrollBodyTo(0, href);
+                    }
+                    else if ($(nameSelector).length != 0) {
+                        scrollBodyTo($(nameSelector).offset().top, href);
+                    }
+                    else {
+                        // fine, we'll just follow the original link. gosh.
+                        window.location = href;
+                    }
+                }
+                else {
+                    scrollBodyTo($(href).offset().top, href);
+                }
+                return false;
+            });
+        }
+        else {
+            $('a[href^="#"]').click(function() {
+                var href = $(this).attr("href");
+
+                if ($(href).length == 0) {
+
+                    var nameSelector = "[name=" + href.replace("#", "") + "]";
+
+                    if (href == "#") {
+                        scrollBodyTo(0, href);
+                    }
+                    else if ($(nameSelector).length != 0) {
+                        scrollBodyTo($(nameSelector).offset().top, href);
+                    }
+                    else {
+                        // fine, we'll just follow the original link. gosh.
+                        window.location = href;
+                    }
+                }
+                else {
+                    scrollBodyTo($(href).offset().top, href);
+                }
+                return false;
+            });
+        }
+    };
+})(jQuery);
+
+$(document).ready(function() {
+    $().SmoothAnchors();
+});
+
 //back to top button
 mybutton = document.getElementById("myBtn");
 

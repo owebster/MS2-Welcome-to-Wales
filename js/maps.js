14111485f2389c5ffda7c61ea5bdbeadb471e912
Owain Webster <owainpw@gmail.com>
2020-12-31 13:52:26 +0000

    added maps marker data points for all card activities

--- js/maps.js
+++ js/maps.js
@@ -38,6 +38,32 @@ function mountainsMarker(){
     marker = new L.marker([51.9667, -3.1167]).addTo(mymap).bindPopup('<p>Black Mountain Range</p>'); //Black Mountain Range
 }
 
+//slate mines
+function slateMineMarkers(){
+    marker = new L.marker([53.1676, -4.0656]).addTo(mymap).bindPopup('<p>Welsh slate penrhyn quarry</p>');
+    marker = new L.marker([52.9976, -3.9693]).addTo(mymap).bindPopup('<p>Cwmorthin Slate Mine</p>');
+}
+
+function activitiesMarkers(){
+    marker = new L.marker([53.168348464480935, -4.0634022615385765]).addTo(mymap).bindPopup('<p>Zip World Penrhyn Quarry</p>');
+    marker = new L.marker([53.1009796051929, -3.7873194172919087]).addTo(mymap).bindPopup('<p>Zip World Fforest</p>');
+    marker = new L.marker([53.00517855612724, -3.9397638154469234]).addTo(mymap).bindPopup('<p>Zip World Slate Caverns</p>');
+    marker = new L.marker([53.0660435464048, -3.7765642749642025]).addTo(mymap).bindPopup('<p>Go Below Underground Adventures</p>');
+    marker = new L.marker([51.66157092432127, -2.6465997866602313]).addTo(mymap).bindPopup('<p>The Wire Zip Slide</p>');
+    marker = new L.marker([52.02586535740562, -3.2151408480151207]).addTo(mymap).bindPopup('<p>Black Mountain Activities</p>');
+    marker = new L.marker([51.562531273635976, -3.725332444334805]).addTo(mymap).bindPopup('<p>Go Ape Margam (Treetop Challenge, Zip Lines, High Ropes)</p>');
+    marker = new L.marker([51.781756229608774, -2.528254388504406]).addTo(mymap).bindPopup('<p>Go Ape Forest of Dean (Mallards Pike - Treetop Challenge and Forest Segways)</p>');
+    marker = new L.marker([51.44886723863433, -3.181802259680882]).addTo(mymap).bindPopup('<p>Cardiff International White Water</p>');
+
+}
+
+function zooMarkers(){
+    marker = new L.marker([53.294632920074, -3.747760530778277]).addTo(mymap).bindPopup('<p>Welsh Mountain Zoo, Colwyn Bay</p>');
+    marker = new L.marker([51.74394105470533, -4.732843630834783]).addTo(mymap).bindPopup('<p>Folly Farm, Pembrokeshire</p>');
+    marker = new L.marker([51.68709497939757, -4.764698657823819]).addTo(mymap).bindPopup('<p>Manor House Wildlife Park, Tenby</p>');
+    marker = new L.marker([53.23509272508752, -4.186551273109475]).addTo(mymap).bindPopup('<p>Pili Palas Nature World, Anglesey</p>');
+    marker = new L.marker([51.41432188514447, -3.3077624731755946]).addTo(mymap).bindPopup('<p>Welsh Hawking Centre</p>');
+}
 
 //on clikc functions for each marker section
 $('.maps-castle-btn').click(function(){
@@ -46,4 +72,16 @@ $('.maps-castle-btn').click(function(){
 
 $('.maps-mountain-btn').click(function(){
     mountainsMarker();
+})
+
+$('.maps-slate-btn').click(function(){
+    slateMineMarkers();
+})
+
+$('.maps-activities-btn').click(function(){
+    activitiesMarkers();
+})
+
+$('.maps-zoos-btn').click(function(){
+    zooMarkers();
 })

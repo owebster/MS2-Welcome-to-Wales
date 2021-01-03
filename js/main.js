9547a7dbbdfb771f5ddd6af03c324f56732568fd
Owain Webster <owainpw@gmail.com>
2021-01-03 13:33:32 +0000

    added content to  cards section for mountains in wales

--- js/main.js
+++ js/main.js
@@ -49,7 +49,7 @@ mybutton = document.getElementById("myBtn");
     }
     }
 
-    // When the user clicks on the button, scroll to the top of the document
+    
     function topFunction() {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera

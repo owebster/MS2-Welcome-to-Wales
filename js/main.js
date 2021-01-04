0c8cf068ab51f1e2ef071d355bc4a6d5a91daecb
Owain Webster <owainpw@gmail.com>
2021-01-04 13:17:51 +0000

    adjusting back to top button smooth scrolling on safari

--- js/main.js
+++ js/main.js
@@ -21,7 +21,6 @@ $('a[href*="#"]')
           scrollTop: target.offset().top
         }, 1000, function() {
           // Callback after animation
-          // Must change focus!
           var $target = $(target);
           $target.focus();
           if ($target.is(":focus")) { // Checking if the target was focused
@@ -51,7 +50,41 @@ mybutton = document.getElementById("myBtn");
 
     
     function topFunction() {
-    document.body.scrollTop = 0; // For Safari
+    document.body.scrollTop = 0;
+    $('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+}); // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
     }
 

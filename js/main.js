bf34b5c0a8bff0749da1c1bed054e2c02e11b20d
Owain Webster <owainpw@gmail.com>
2021-01-09 09:11:40 +0000

    added smooth scrolling js into main js file to reduce the amount of files

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
+//smooth scrolling for safari
+$(document).ready(function(){
+    $('a').on('click', function(event){
+      if(this.hash !== "") {
+        event.preventDefault();
+        var hash = this.hash;
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top
+        }, 800, function(){
+  
+          window.location.hash = hash;
+        });
+      }
+    });
+  });
+
 //back to top button
 
   mybutton = document.getElementById("myBtn");

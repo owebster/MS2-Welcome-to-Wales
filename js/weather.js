5c106b8d8da284d5891729a34d75ea707729739a
Owain Webster <owainpw@gmail.com>
2021-01-09 09:10:58 +0000

    testing weather api in weather.html file

--- js/weather.js
+++ js/weather.js
@@ -1,12 +1,21 @@
 // http://api.weatherstack.com/current?access_key=be6ea8e1d22896aa92288375aa53cb4f&query=Cardiff
-var xhr = new XMLHttpRequest();
+const baseUrl = "http://api.weatherstack.com/current?access_key=be6ea8e1d22896aa92288375aa53cb4f&query=Cardiff";
 
-xhr.open("GET", "http://api.weatherstack.com/current?access_key=be6ea8e1d22896aa92288375aa53cb4f&query=Cardiff")
+function getData(type, cb){
+    var xhr = new XMLHttpRequest();
 
-xhr.send();
+    xhr.open("GET", baseUrl + type + "/")
+    xhr.send();
+    
+    xhr.onreadystatechange = function(){
+        if(this.readyState == 4 && this.status == 200) {
+            cb(JSON.parse(this.responseText));
+        };
+    };
+}
 
-xhr.onreadystatechange = function(){
-    if(this.readyState == 4 && this.status == 200) {
-        console.log(JSON.parse(this.responseText));
-    }
-};
+function writeToDocument(type){
+    getData(type, function(data){
+        document.getElementById("weatherData").innerHTML = data;
+    })
+}

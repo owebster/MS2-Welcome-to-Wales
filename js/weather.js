1294d30967f870cb133226bce7d70d8c3c961d24
Owain Webster <owainpw@gmail.com>
2021-01-09 11:08:21 +0000

    added darksky api with skycons (designed by darksky) for animated icons

--- js/weather.js
+++ js/weather.js
@@ -1,21 +1,27 @@
 // http://api.weatherstack.com/current?access_key=be6ea8e1d22896aa92288375aa53cb4f&query=Cardiff
-const baseUrl = "http://api.weatherstack.com/current?access_key=be6ea8e1d22896aa92288375aa53cb4f&query=Cardiff";
+// darksky api https://api.darksky.net/forecast/5d55e2e606650c0ef689fa5fd436c424/51.4816,-3.1791
 
-function getData(type, cb){
-    var xhr = new XMLHttpRequest();
+window.addEventListener('load', ()=> {
 
-    xhr.open("GET", baseUrl + type + "/")
-    xhr.send();
-    
-    xhr.onreadystatechange = function(){
-        if(this.readyState == 4 && this.status == 200) {
-            cb(JSON.parse(this.responseText));
-        };
-    };
-}
+    let lat = 51.4816;
+    let long = -3.1791; // cardiff lat long
+    let weatherIcon = document.querySelector('.weatherIcon');
+    let temperatureDegree = document.querySelector('.temperatureDegree')
+    let currentDescription = document.querySelector('.currentDescription')
+    let locationTimezone = document.querySelector('.locationTimezone')
 
-function writeToDocument(type){
-    getData(type, function(data){
-        document.getElementById("weatherData").innerHTML = data;
-    })
-}
+    const proxy = "https://cors-anywhere.herokuapp.com/";
+    const api = `${proxy}https://api.darksky.net/forecast/5d55e2e606650c0ef689fa5fd436c424/${lat},${long}`;
+
+    fetch(api)
+        .then(response => {
+            return response.json();
+        })
+        .then(data => {
+            console.log(data);
+            const {temperature, summary} = data.currently;
+            temperatureDegree.textContent = temperature;
+            currentDescription.textContent = summary;
+            locationTimezone.textContent = data.timezone;
+        });
+});

240c99f11715085c986748252c21d2843892d918
Owain Webster <owainpw@gmail.com>
2021-01-14 21:12:59 +0000

    added new tags for weather descriptions

--- js/weather.js
+++ js/weather.js
@@ -1,15 +1,9 @@
-// http://api.weatherstack.com/current?access_key=be6ea8e1d22896aa92288375aa53cb4f&query=Cardiff
-// darksky api https://api.darksky.net/forecast/5d55e2e606650c0ef689fa5fd436c424/51.4816,-3.1791
-
 //openweather
 //https://api.openweathermap.org/data/2.5/weather?q=Cardiff&appid=983533143e870dc758b970f7b3eb380b
 
 window.addEventListener('load', ()=> {
     /*let lat = 51.4816;
     let long = -3.1791; // cardiff lat long*/
-    let temperatureDegree = document.querySelector('.temperatureDegree')
-    let currentDescription = document.querySelector('.currentDescription')
-    //let weatherIcon = document.querySelector('.weatherIcon')
     const api = 'https://api.openweathermap.org/data/2.5/weather?q=Cardiff&units=metric&appid=983533143e870dc758b970f7b3eb380b';
 
     fetch(api)
@@ -18,9 +12,9 @@ window.addEventListener('load', ()=> {
         })
         .then(data => {
             console.log(data);
-            const {main} = data;
-            temperatureDegree.textContent = Math.round(main.temp);
-            //currentDescription.textContent = ;
-            //weatherIcon.src = weather_icons;
+            const {main, name, weather} = data;
+            $('.weather-city').html(`<h2>${name}</h2>`)
+            $('.temperatureDegree').html(`<h3>Currently it's: ${Math.round(main.temp)}˚C and ${weather[0].description}</h3>`)
+            $('.feelsLike').html(`<h3>Feels Like: ${Math.round(main.feels_like)}˚C</h3>`)
         });
 });

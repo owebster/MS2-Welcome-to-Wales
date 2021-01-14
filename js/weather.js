e40df618f89ebb2e71fe5ca27ed347fa4a38f714
Owain Webster <owainpw@gmail.com>
2021-01-14 15:20:36 +0000

    replaced darksky api with weatherstack api due to darksky issue

--- js/weather.js
+++ js/weather.js
@@ -2,14 +2,15 @@
 // darksky api https://api.darksky.net/forecast/5d55e2e606650c0ef689fa5fd436c424/51.4816,-3.1791
 
 window.addEventListener('load', ()=> {
-
     /*let lat = 51.4816;
     let long = -3.1791; // cardiff lat long*/
     let temperatureDegree = document.querySelector('.temperatureDegree')
     let currentDescription = document.querySelector('.currentDescription')
+    let weatherIcon = document.querySelector('.weatherIcon')
     //const proxy = "https://cors-anywhere.herokuapp.com/";
-    //const api = `${proxy}https://api.darksky.net/forecast/5d55e2e606650c0ef689fa5fd436c424/${lat},${long}`;
-    const api = `https://api.darksky.net/forecast/5d55e2e606650c0ef689fa5fd436c424/51.4816,-3.1791`;
+    //const api = `${proxy}https://api.darksky.net/forecast/5d55e2e606650c0ef689fa5fd436c424/51.4816, -3.1791`;
+    //const api = `https://api.darksky.net/forecast/5d55e2e606650c0ef689fa5fd436c424/51.4816,-3.1791`;
+    const api = 'http://api.weatherstack.com/current?access_key=be6ea8e1d22896aa92288375aa53cb4f&query=Cardiff';
 
     fetch(api)
         .then(response => {
@@ -17,17 +18,9 @@ window.addEventListener('load', ()=> {
         })
         .then(data => {
             console.log(data);
-            const {temperature, summary, icon} = data.currently;
-            temperatureDegree.textContent = Math.floor((temperature - 32) * (5 / 9));
-            currentDescription.textContent = summary;
-            
-            // set icons
-            setIcons(icon, document.querySelector('.weatherIcon'));
+            const {temperature, weather_descriptions, weather_icons} = data.current;
+            temperatureDegree.textContent = temperature;
+            currentDescription.textContent = weather_descriptions;
+            weatherIcon.src = weather_icons;
         });
-    function setIcons(icon, iconID){
-        const skycons = new Skycons({color: "black"});
-        const currentIcon = icon.replace(/-/g, "_").toUpperCase();
-        skycons.play()
-        return skycons.set(iconID, Skycons[currentIcon])
-    }
 });

7a488debd8704baf849d9140484589db56190079
Owain Webster <owainpw@gmail.com>
2021-01-15 10:00:47 +0000

    added basic styling to second weather api call

--- js/weather.js
+++ js/weather.js
@@ -3,8 +3,10 @@
 /*let lat = 51.4816; 
 let long = -3.1791; cardiff lat long*/
 
+let southWales = 'Cardiff';
+let northWales = 'Holyhead';
 window.addEventListener('load', function() {
-    const api = 'https://api.openweathermap.org/data/2.5/weather?q=Cardiff&units=metric&appid=983533143e870dc758b970f7b3eb380b';
+    const api = `https://api.openweathermap.org/data/2.5/weather?q=${southWales}&units=metric&appid=983533143e870dc758b970f7b3eb380b`;
 
     fetch(api)
         .then(function(response) {
@@ -15,6 +17,19 @@ window.addEventListener('load', function() {
             const {main, name, weather} = data;
             $('.weather-city').html(`<h2>${name}</h2>`).css("color", "#D50130");
             $('.temperatureDegree').html(`<h3>Currently it's: ${Math.round(main.temp)}˚C with a low of ${Math.round(main.temp_min)}˚C </h3>`);
-            $('.feelsLike').html(`<h3>Feels Like: ${Math.round(main.feels_like)}˚C and ${weather[0].description}</h3>`);
+            $('.feelsLike').html(`<h3>Feels Like: ${Math.round(main.feels_like)}˚C with ${weather[0].description}</h3>`);
         });
 });
+
+window.addEventListener('load', function() {
+    const api = `https://api.openweathermap.org/data/2.5/weather?q=${northWales}&units=metric&appid=983533143e870dc758b970f7b3eb380b`;
+
+    fetch(api)
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(data){
+            const {main, name, weather} = data;
+            
+        })
+})

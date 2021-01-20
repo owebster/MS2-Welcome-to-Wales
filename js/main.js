ad5dea76230fb0f1e1ff1895715c43869a1ba8cd
Owain Webster <owainpw@gmail.com>
2021-01-20 14:26:48 +0000

    added missing semicolon to line 21 of main.js file

--- js/main.js
+++ js/main.js
@@ -18,7 +18,7 @@ $(document).ready(function(){
   mybutton = document.getElementById("myBtn");
 
       // When the user scrolls down 20px from the top of the document, show the button
-      window.onscroll = function(){scrollFunction()};
+      window.onscroll = function(){scrollFunction();};
 
       function scrollFunction() {
       if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {

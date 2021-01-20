7074ac3ccb69b19a7eb92ad96104f695b9004dde
Owain Webster <owainpw@gmail.com>
2021-01-20 12:05:58 +0000

    added additon comments to activity functions for easier reading

--- js/maps.js
+++ js/maps.js
@@ -41,35 +41,35 @@ function mountainsMarker(){
 
 //slate mines
 function slateMineMarkers(){
-    marker = new L.marker([53.0053, -3.9404]).addTo(mapMarkerGroup).bindPopup('<p>Llechwedd Slate Quarry</p>');
+    marker = new L.marker([53.0053, -3.9404]).addTo(mapMarkerGroup).bindPopup('<p>Llechwedd Slate Quarry</p>'); //Llechwedd Slate Quarry
 }
 
 function activitiesMarkers(){
-    marker = new L.marker([53.168348464480935, -4.0634022615385765]).addTo(mapMarkerGroup).bindPopup('<p>Zip World Penrhyn Quarry</p>');
-    marker = new L.marker([53.1009796051929, -3.7873194172919087]).addTo(mapMarkerGroup).bindPopup('<p>Zip World Fforest</p>');
-    marker = new L.marker([53.00517855612724, -3.9397638154469234]).addTo(mapMarkerGroup).bindPopup('<p>Zip World Slate Caverns</p>');
-    marker = new L.marker([53.0660435464048, -3.7765642749642025]).addTo(mapMarkerGroup).bindPopup('<p>Go Below Underground Adventures</p>');
-    marker = new L.marker([51.66157092432127, -2.6465997866602313]).addTo(mapMarkerGroup).bindPopup('<p>The Wire Zip Slide</p>');
-    marker = new L.marker([52.02586535740562, -3.2151408480151207]).addTo(mapMarkerGroup).bindPopup('<p>Black Mountain Activities</p>');
-    marker = new L.marker([51.562531273635976, -3.725332444334805]).addTo(mapMarkerGroup).bindPopup('<p>Go Ape Margam (Treetop Challenge, Zip Lines, High Ropes)</p>');
-    marker = new L.marker([51.781756229608774, -2.528254388504406]).addTo(mapMarkerGroup).bindPopup('<p>Go Ape Forest of Dean (Mallards Pike - Treetop Challenge and Forest Segways)</p>');
-    marker = new L.marker([51.44886723863433, -3.181802259680882]).addTo(mapMarkerGroup).bindPopup('<p>Cardiff International White Water</p>');
+    marker = new L.marker([53.168348464480935, -4.0634022615385765]).addTo(mapMarkerGroup).bindPopup('<p>Zip World Penrhyn Quarry</p>');//Zip World Penrhyn Quarry
+    marker = new L.marker([53.1009796051929, -3.7873194172919087]).addTo(mapMarkerGroup).bindPopup('<p>Zip World Fforest</p>');//Zip World Fforest
+    marker = new L.marker([53.00517855612724, -3.9397638154469234]).addTo(mapMarkerGroup).bindPopup('<p>Zip World Slate Caverns</p>');//Zip World Slate Caverns
+    marker = new L.marker([53.0660435464048, -3.7765642749642025]).addTo(mapMarkerGroup).bindPopup('<p>Go Below Underground Adventures</p>');//Go Below Underground Adventures
+    marker = new L.marker([51.66157092432127, -2.6465997866602313]).addTo(mapMarkerGroup).bindPopup('<p>The Wire Zip Slide</p>');//The Wire Zip Slide
+    marker = new L.marker([52.02586535740562, -3.2151408480151207]).addTo(mapMarkerGroup).bindPopup('<p>Black Mountain Activities</p>');//Black Mountain Activities
+    marker = new L.marker([51.562531273635976, -3.725332444334805]).addTo(mapMarkerGroup).bindPopup('<p>Go Ape Margam (Treetop Challenge, Zip Lines, High Ropes)</p>');//Go Ape Margam (Treetop Challenge, Zip Lines, High Ropes)
+    marker = new L.marker([51.781756229608774, -2.528254388504406]).addTo(mapMarkerGroup).bindPopup('<p>Go Ape Forest of Dean (Mallards Pike - Treetop Challenge and Forest Segways)</p>');//Go Ape Forest of Dean (Mallards Pike - Treetop Challenge and Forest Segways)
+    marker = new L.marker([51.44886723863433, -3.181802259680882]).addTo(mapMarkerGroup).bindPopup('<p>Cardiff International White Water</p>');//Cardiff International White Water
 
 }
 
 function zooMarkers(){
-    marker = new L.marker([53.294632920074, -3.747760530778277]).addTo(mapMarkerGroup).bindPopup('<p>Welsh Mountain Zoo, Colwyn Bay</p>');
-    marker = new L.marker([51.74394105470533, -4.732843630834783]).addTo(mapMarkerGroup).bindPopup('<p>Folly Farm, Pembrokeshire</p>');
-    marker = new L.marker([51.68709497939757, -4.764698657823819]).addTo(mapMarkerGroup).bindPopup('<p>Manor House Wildlife Park, Tenby</p>');
-    marker = new L.marker([53.23509272508752, -4.186551273109475]).addTo(mapMarkerGroup).bindPopup('<p>Pili Palas Nature World, Anglesey</p>');
-    marker = new L.marker([51.41432188514447, -3.3077624731755946]).addTo(mapMarkerGroup).bindPopup('<p>Welsh Hawking Centre</p>');
+    marker = new L.marker([53.294632920074, -3.747760530778277]).addTo(mapMarkerGroup).bindPopup('<p>Welsh Mountain Zoo, Colwyn Bay</p>');//Welsh Mountain Zoo, Colwyn Bay
+    marker = new L.marker([51.74394105470533, -4.732843630834783]).addTo(mapMarkerGroup).bindPopup('<p>Folly Farm, Pembrokeshire</p>');//Folly Farm, Pembrokeshire
+    marker = new L.marker([51.68709497939757, -4.764698657823819]).addTo(mapMarkerGroup).bindPopup('<p>Manor House Wildlife Park, Tenby</p>');//Manor House Wildlife Park, Tenby
+    marker = new L.marker([53.23509272508752, -4.186551273109475]).addTo(mapMarkerGroup).bindPopup('<p>Pili Palas Nature World, Anglesey</p>');//Pili Palas Nature World, Anglesey
+    marker = new L.marker([51.41432188514447, -3.3077624731755946]).addTo(mapMarkerGroup).bindPopup('<p>Welsh Hawking Centre</p>');//Welsh Hawking Centre
 }
 
 function deleteMarkers(){
     mapMarkerGroup.clearLayers(mymap);
 }
 
-//on clikc functions for each marker section
+//on click functions for each marker section
 $('.maps-castle-btn').click(function(){
     deleteMarkers();
     castleMarker();
